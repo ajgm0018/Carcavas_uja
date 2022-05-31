3587fa01bc72f5516cdafd554f31772808f003b2
ajgm0018 <ajgm0018@red.ujaen.es>
2022-05-31 11:07:35 +0200

    Add files via upload

--- ModeloGeneralFinal.py
+++ ModeloGeneralFinal.py
@@ -7,6 +7,10 @@ Created on Thu May  5 11:58:50 2022
 
 # -- LIBRERIAS Y OPCIONES -- #
 
+"""
+Librerías utilizadas
+"""
+
 import os
 import pandas as pd
 from osgeo import gdal
@@ -32,29 +36,43 @@ from sklearn.model_selection import cross_validate
 
 # -- FUNCIONES - #
 
+"""
+-- Inicio de las funciones de carga y tratamiento de datos --
+"""
+
+"""
+Carga de datos
+
+@param: path -> directorio donde se encuentran los datos a cargar de la zona a la que se refiere la función
+@return: datos
+"""
 def cargar_datos_SantoTome(path):
     print("\n-- Cargando datos Santo Tome --")
     dir = path
     ficheros=[]
     nombre=[]
     
+    # Recorre todos los archivos del directorio y recoge solo los archivos con terminación .tif
     for file in os.listdir(dir):
         if file.endswith(".tif"):
             ficheros.append(os.path.join(dir, file))
             nombre.append(file.split('.')[0])
-            
+
+    # Ordenado los ficheros y los nombres de los mismos por orden alfabético
     sorted(ficheros)
     sorted(nombre)
     
+    # Crea 3 dataframe con las columnas de los nombres de las variables
+    # Datos se utilizará para todo el proceso de aprendizaje
     datos = pd.DataFrame(columns=nombre)
-    datos_sin_tocar = pd.DataFrame(columns=nombre)
-    datos_para_dibujado = pd.DataFrame(columns=nombre)
     
+    # Abrimos uno de los archivos TIF con la librería GDAL para obtener las dimensiones de las variables
     tif = gdal.Open(ficheros[1])
     tif_band = tif.GetRasterBand(1)
     x = tif_band.XSize
     y = tif_band.YSize
     
+    # Recorremos cada uno de los ficheros del directorio
     contador = 0
     for f in ficheros:
         n = nombre[contador]
@@ -76,26 +94,32 @@ def cargar_datos_SantoTome(path):
 
         # Estos serán nuestros datos
         datos[n] = band
-        datos_sin_tocar[n] = band
-        datos_para_dibujado[n] = band
 
         contador = contador + 1
     
-    #return datos, datos_para_dibujado, datos_sin_tocar, x, y
+    # Devolvemos los datos
     return datos
 
+"""
+Tratamiemto de los datos
+
+@param: datos -> datos a tratar, en este caso deben de ser de Santo Tomé
+@return: datos
+"""
 def tratamiento_datos_SantoTome(datos):
     print("\n-- Tratamiento datos Santo Tome --\n")
     print("Número de datos antes del tratamiento ", datos.size)
 
+    # Se eliminan todos los datos que coinciden con los valores mostrados a continuación
+    # Ej. Solo queremos los datos donde la distancia de carreteras sea mayor o igual que 0 el resto serán datos corruptos
     datos = datos[datos['Carcavas'] != 128]
     datos = datos[datos['Arcillas'] >= 0]
     datos = datos[datos['Distancia_Carreteras'] >= 0]
-    # -- MIRAR GEOLOGIA -- #
     datos = datos[datos['Geologia'] != 0]
     datos = datos[datos['Orientaciones'] >= 0]
     
     print("Número de datos despues del tratamiento ", datos.size)   
+    # Tratamiento de valores categóricos dependiendo del estándar establecido
     
     # Geología
     datos.loc[datos.Geologia == 1, "Geologia"] = "Codigo_9131"
@@ -119,10 +143,14 @@ def tratamiento_datos_SantoTome(datos):
     datos.loc[datos.Unidades_Edaficas == 23, "Unidades_Edaficas"] = "Codigo_23"
     datos.loc[datos.Unidades_Edaficas == 22, "Unidades_Edaficas"] = "Codigo_22"
 
+    # Redondeo de los datos
     datos = datos.round(4)
+    
+    # En este caso se eliminan las cárcavas de 2011 no se van a utilizar
     del datos['Carcavas_2011']
     
     # Label Encoder
+    # Damos a los valores categóricos como categoría
     datos["Geologia"] = datos["Geologia"].astype("category")
     datos["Usos_Del_Suelo"] = datos["Usos_Del_Suelo"].astype("category")
     datos["Unidades_Edaficas"] = datos["Unidades_Edaficas"].astype("category")
@@ -131,10 +159,16 @@ def tratamiento_datos_SantoTome(datos):
     
     le = LabelEncoder()
     
+    # Aplicamos label encoder par aque puedan ser tratados por el modelo
     datos[categorical_cols] = datos[categorical_cols].apply(lambda col: le.fit_transform(col))    
 
+    # Devolvemos los datos tratados
     return datos
 
+"""
+Las funciones carga de datos y tratamiento de datos son semejantes a las documentadas anteriormente
+con pequeñas diferencias en la carga y en los tratamientos.
+"""
 def cargar_datos_Berrueco(path):
     print("\n-- Cargando datos Berrueco --")
     dir = path
@@ -150,8 +184,6 @@ def cargar_datos_Berrueco(path):
     sorted(nombre)
     
     datos = pd.DataFrame(columns=nombre)
-    datos_sin_tocar = pd.DataFrame(columns=nombre)
-    datos_para_dibujado = pd.DataFrame(columns=nombre)
     
     tif = gdal.Open(ficheros[1])
     tif_band = tif.GetRasterBand(1)
@@ -179,12 +211,9 @@ def cargar_datos_Berrueco(path):
 
         # Estos serán nuestros datos
         datos[n] = band
-        datos_sin_tocar[n] = band
-        datos_para_dibujado[n] = band
 
         contador = contador + 1
-    
-    #return datos, datos_para_dibujado, datos_sin_tocar, x, y    
+       
     return datos
 
 def tratamiento_datos_Berrueco(datos):
@@ -236,6 +265,11 @@ def tratamiento_datos_Berrueco(datos):
 
     return datos
 
+"""
+En esta carga de datos hay una diferencia, al igual que en Rentillas
+
+@resume: Observar código
+"""
 def cargar_datos_Lupion(path):
     print("\n-- Cargando datos Lupion --")
     dir = path
@@ -251,8 +285,6 @@ def cargar_datos_Lupion(path):
     sorted(nombre)
     
     datos = pd.DataFrame(columns=nombre)
-    datos_sin_tocar = pd.DataFrame(columns=nombre)
-    datos_para_dibujado = pd.DataFrame(columns=nombre)
     
     tif = gdal.Open(ficheros[1])
     tif_band = tif.GetRasterBand(1)
@@ -270,7 +302,11 @@ def cargar_datos_Lupion(path):
         # Pasamos los datos a float
         band = MDT.GetRasterBand(1).ReadAsArray().astype(float)
 
-        # Se desechan las dos primeras filas y columnas debido a calculos de borde
+        """
+        Como algunas de las capas no coinciden en las dimensiones proporcionadas,
+        se recortan los bordes dependiendo de estas dimensiones para crear un estandar entre
+        las variables
+        """
         if(n == "Carcavas" or n == "Lupi_11_9999"): 
             band = band[:, 1:x]
         elif (n == "Curvatura_Perfil" or n == "Curvatura_Plana" or n == "Distancia_Carreteras"
@@ -286,12 +322,9 @@ def cargar_datos_Lupion(path):
 
         # Estos serán nuestros datos
         datos[n] = band
-        datos_sin_tocar[n] = band
-        datos_para_dibujado[n] = band
 
         contador = contador + 1
     
-    #return datos, datos_para_dibujado, datos_sin_tocar, x, y
     return datos
 
 def tratamiento_datos_Lupion(datos):
@@ -468,12 +501,27 @@ def tratamiento_datos_Rentillas(datos):
     
     return datos
 
+"""
+-- Fin de las funciones de carga y tratamiento de datos --
+"""
+
+"""
+Union de los datos de zonas diferentes en un mismo Dataframe
+
+@param: datos_X -> datos de las zonas tratadas
+@return: datos
+"""
 def union_datos(datos_SantoTome, datos_Berrueco, datos_Lupion, datos_Rentillas):
     print("\n-- Uniendo datos --")
     frames = [datos_SantoTome, datos_Berrueco, datos_Lupion, datos_Rentillas]
     datos = pd.concat(frames, ignore_index=True)
     return datos
 
+"""
+Creación de matriz de correlación entre las variables
+
+@param: datos -> datos de los que quieres sacar la correlación de sus instancias/variables
+"""
 def matriz_correlacion(datos):
     print("-- Matriz de confusion --")
     corr_datos = datos.corr(method='pearson')
@@ -481,29 +529,14 @@ def matriz_correlacion(datos):
     sns.heatmap(corr_datos, annot=True)
     plt.show()
 
+"""
+Sacar por pantalla el VIF de las instancias/variables de los datos
+
+@param: datos -> datos de los que quieres sacar el VIF de sus instancias/variables
+"""
 def vif(datos):
 
     print("\n-- Realizando VIF --\n")
-    
-    """
-    # Mapeamos
-    datos['Geologia'] = datos['Geologia'].map({
-        'Codigo_9002':0, 'Codigo_9133':1, 'Codigo_9001':2, 'Codigo_9330':3,
-        'Codigo_9131':4, 'Codigo_9132':5, 'Codigo_9201':6, 'Codigo_9103':7,
-        'Codigo_9202':8, 'Codigo_9004':9, 'Codigo_9134':10, 'Codigo_9000':11,
-        'Codigo_9003':12, 'Codigo_8996':13, 'Codigo_9102':14})
-    
-    datos['Usos_Del_Suelo'] = datos['Usos_Del_Suelo'].map({
-        'Tierras_regadas':0, 'Olivares':1, 'Labor_secano':2, 'Cultivos_permanentes':3,
-        'Tejido_urbano':4, 'Pastizales':5, 'Mosaicos_cultivos':6, 'Cursos_agua':7,
-        'Frutales':8})
-    
-    datos['Unidades_Edaficas'] = datos['Unidades_Edaficas'].map({
-        'Codigo_22':0, 'Codigo_48':1, 'Codigo_44':2, 'Codigo_23':3,
-        'Codigo_13':4, 'Codigo_21':5, 'Codigo_11':6, 'Codigo_49':7,
-        'Codigo_2':8, 'Codigo_42':9, 'Codigo_58':10, 'Codigo_47':11,
-        'Codigo_37':12})
-    """
     
     # VIF dataframe
     vif_data = pd.DataFrame()
@@ -516,6 +549,17 @@ def vif(datos):
     print("\n")
     print(vif_data)
 
+"""
+Definición del modelo XGBoost que se utilizará
+
+@param: learning rate -> ratio de aprendizaje
+@param: depth -> profundidad de arbol
+@param: estimators -> estimadores
+@param: valor_desequilibrio -> valor para modelos desbalanceados
+@return: modelo
+
+Fuente: https://xgboost.readthedocs.io/en/stable/parameter.html
+"""
 def modelo_XGBoost(learning_rate, depth, estimators, valos_desequilibrio):
   modelo_xgb = xgb.XGBClassifier(use_label_encoder=False, 
                                  verbosity=0, 
@@ -524,17 +568,40 @@ def modelo_XGBoost(learning_rate, depth, estimators, valos_desequilibrio):
                                  sampling_method='gradient_based',
                                  scale_pos_weight=valos_desequilibrio,
                                  n_estimators=estimators)
+  # Devuelve el modelo
   return modelo_xgb  
 
+"""
+Definicion de la importancia de pesos que han tenido las variables en el modelo de aprendizaje
+
+@param: modelo -> modelo entrenado
+"""
 def importancia_variables(modelo):
     plot_importance(modelo)
     plt.show()
 
+"""
+Obtención de True Positive, False Negative, False Positive y True Negative de una matriz
+de confusión
+
+@param: Y_test -> variable dependiente de testeo
+@param: Y_pred -> variable dependiente predicha
+@return: True Positive, False Negative, False Positive y True Negative
+"""
 def m_confusion(Y_test, Y_pred):
     cnf_matrix = confusion_matrix(Y_test, Y_pred, labels=[1,0])
     tp, fn, fp, tn = cnf_matrix.ravel()
     return tp, fn, fp, tn
 
+"""
+Obtención del coeficiente Kappa
+
+@param: TP -> True Positive
+@param: FP -> False Positive
+@param: FN -> False Negative
+@param: TN -> True Negative
+@return: Kappa
+"""
 def kappa(TP, FP, FN, TN):
     N = TP+TN+FP+FN
 
@@ -548,14 +615,31 @@ def kappa(TP, FP, FN, TN):
     
     return k
 
+"""
+Devuelve el valor Kappa y el valor de aciertos positivos
+
+@param: modelo -> modelo creado sin entrenar
+@param: x_pred -> valores a predecir
+@param: y_P    -> variable dependiente de los valores a predecir
+@return: Kappa, True Positive, RMSE, ROC
+"""
 def validaciones_modelo(modelo, x_pred, y_P):
     
     y_pred = modelo.predict(x_pred)
     tp, fn, fp, tn = m_confusion(y_P, y_pred)
     k = kappa(tp, fp, fn, tn)
+    
+    rmse, roc = rmse_auc(modelo, x_pred, y_P, y_pred)
 
-    return k, tp
+    return k, tp, rmse, roc
 
+"""
+Elimina de los datos las variables seleccionadas en la función 
+(realizar manualmente en la funcion)
+
+@param: datos -> datos con las variables a eliminar
+@return: datos
+"""
 def eliminacion_variables(datos):
     
     del datos['Unidades_Edaficas']
@@ -566,8 +650,17 @@ def eliminacion_variables(datos):
     
     return datos
 
-def rmse_auc(modelo, test_x, test_y):
-    pred = modelo.predict(test_x)
+"""
+Realiza RSME y Curve ROC
+
+@param: modelo -> modelo entrenado (si quieres utilizar esta funcion a parte, descomentar primera linea)
+@param: test_x -> valores a predecir (si utilizas la funcion a parte)
+@param: test_y -> variable dependiente de los valores predichos
+@param: pred   -> resultado de la predicción del modelo
+@return: rmse y roc
+"""
+def rmse_auc(modelo, test_x, test_y, pred):
+    #pred = modelo.predict(test_x)
     rmse = np.sqrt(MSE(test_y, pred))
     
     y_pred = modelo.predict_proba(test_x)[:,1]
@@ -575,10 +668,19 @@ def rmse_auc(modelo, test_x, test_y):
     
     return rmse, roc
 
+"""
+Validación cruzada en 10 splits
+
+@param: x -> variables independientes
+@param: y -> variable/s dependiente/s
+@param: modelo -> modelo definido
+@return: resultados
+"""
 def validacion_cruzada(x, y, modelo):
     print("\n-- Validacion cruzada --\n")
     kfold = StratifiedKFold(n_splits=10)
     
+    # Resultados que quieres observar en la validacion
     scoring = {'mse': 'neg_mean_squared_error',
                'precision_desbalanceada': 'balanced_accuracy',
                'precision': 'accuracy',
@@ -592,6 +694,9 @@ def validacion_cruzada(x, y, modelo):
 
 # -- PARAMETROS -- #
 """
+Parametros utilizados para los diferentes modelos a crear, desde aquí se pueden
+cambiar las variables que utiliza
+
 - Modelo 1: Learning rate: 0.10, Profundidad 6 y número de estimadores 150
 - Modelo 2: Learning rate: 0.25, Profundidad 10 y número de estimadores 250
 """
@@ -601,6 +706,14 @@ profundidad = 6
 estimadores = 150
 
 # -- MAIN -- #
+"""
+--- IMPORTANTE ---
+
+El programa no sigue una secuencialidad. Algunas partes vendrán detalladas para ser opcionalmente
+comentadas, oprimidas o utilizadas
+
+--- IMPORTANTE ---
+"""
 
 # Cargamos los datos y tratamiento de -- SANTO TOME --
 dir_SantoTome = "Raster/SantoTome_final"
@@ -641,24 +754,29 @@ del datos_Rentillas
 datos = eliminacion_variables(datos)
 
 
-
 # Matriz de correlacion de datos
-# matriz_correlacion(datos)
+matriz_correlacion(datos)
 
 
 # Correlacion a través de VIF
-# vif(datos)
+vif(datos)
 
 
 # Separacion de datos en X e Y
 Y = datos.Carcavas
 datos = datos.drop(['Carcavas'], axis=1)
 X = datos
+
+
 # Dividimos el dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 2)
 valos_desequilibrio = math.sqrt(Y.value_counts()[0]/Y.value_counts()[1])
 
-
+"""
+-- IMPORTANTE --
+Descomentar esta parte si tienes ya un modelo guardado, no sería necesario la creación
+ni entrenamiento de otro modelo
+"""
 # Cargar modelos
 # modelo = pickle.load(open("Modelos/modelo_general_1_variables", "rb"))
 
@@ -668,7 +786,7 @@ print("\n-- Creando modelo --")
 modelo = modelo_XGBoost(eta, profundidad, estimadores, valos_desequilibrio)
 
 
-# Validacion cruzada
+# Validacion cruzada -- INICIO --
 resultados = validacion_cruzada(X, Y, modelo)
 print(resultados.keys())
 
@@ -689,26 +807,39 @@ mse = - resultados['train_mse'].mean()
 print("RMSE: ", math.sqrt(mse))
 print("ROC: ", resultados['train_roc_auc'].mean())
 print("F1: ", resultados['train_f1'].mean())
+# Validacion cruzada -- FINAL --
+
 
 """
+-- IMPORTANTE --
+Con la validación cruzada ya has entrenado el modelo el numero de splits correspondientes
+por lo tanto no necesitas utilizar otro entrenamiento del modelo, si quieres entrenarlo a parte
+comentar todo el apartado de la validación cruzada
+
+-- IMPORTANTE -- 
+Si has realizado la validación cruzada puedes comentar todo el código que hay a partir de aquí
+"""
+
 # Entrenamiento del modelo
 print("\n-- Entrenando modelo --")
 modelo.fit(X_train, Y_train)
 
 
+"""
+-- IMPORTANTE --
+Esto guarda el modelo entrenado, con la validación cruzada esto no sirve, descomentar si se quiere
+guardar el modelo y se ha utilizado -> modelo.fit
+"""
 # Guardamos modelo
 # pickle.dump(modelo, open("Modelos/modelo_general_2_variablesxxxx", "wb"))
 
-
 # Feature important
-# importancia_variables(modelo)
-
+importancia_variables(modelo)
 
 # Validacion
 print("\n-- Validacion... --\n")
-k, tp = validaciones_modelo(modelo, X_test, Y_test)
+k, tp, rmse_valor, roc = validaciones_modelo(modelo, X_test, Y_test)
 print("Kappa: ", k)
-rmse_valor, roc = rmse_auc(modelo, X_test, Y_test)
 print("RMSE: ", rmse_valor, "ROC ", roc)
-"""
+
 
